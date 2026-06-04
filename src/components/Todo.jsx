acfa8d8bb2c6494642e993c0822468b1a4ce72d4
Abolfazl <slymanawjaqy@gmail.com>
2026-06-04 14:51:36 +0330

    style: adjust todo component width for large screens

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,7 +2,7 @@ import { Trash2, CheckCheck } from "lucide-react"
 const Todo = ({ id, title, isDone, onDelete, onDone, priority }) => {
     const priorityTextStyle = priority === "Important" ? "text-red-600" : "text-emerald-500"
     return (
-        <div className="lg:w-250 bg-white px-8 max-sm:px-3 py-2 rounded-lg sm:text-lg font-semibold flex justify-between items-center dark:bg-gray-800 shadow-2xl hover:shadow-lg dark:shadow-gray-800 duration-600 md:w-180 sm:w-155 max-sm:w-screen">
+        <div className="lg:w-225 bg-white px-8 max-sm:px-3 py-2 rounded-lg sm:text-lg font-semibold flex justify-between items-center dark:bg-gray-800 shadow-2xl hover:shadow-lg dark:shadow-gray-800 duration-600 md:w-180 sm:w-155 max-sm:w-screen">
             <p>{title}</p>
             <p className={priorityTextStyle}>{priority}</p>
             <div className="flex gap-x-2 items-center">
